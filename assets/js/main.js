45378a61d17ce3c5b07c1e3ce23dbb22ccdef05e
Anshul Tomar <108689719+anshultomar07@users.noreply.github.com>
2022-12-10 21:50:29 +0530

    Changed reveal time

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,30 +55,26 @@ sr.reveal('.home__scroll', {delay: 200})
 sr.reveal('.home__img', {origin:'right', delay: 400})
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__img', {delay: 20})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
-sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__text', {delay: 20})
 sr.reveal('.about__social-icon', {delay: 600, interval: 200})
 
 /*SCROLL EDUCATION*/
-sr.reveal('.education__subtitle', {delay: 300})
-sr.reveal('.education__text', {delay: 500})
+sr.reveal('.education__subtitle', {delay: 10})
+sr.reveal('.education__text', {delay: 30})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
-sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__img', {delay: 20})
 
-/*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', {interval: 200})
+/*SCROLL PROJECT*/
+sr.reveal('.portfolio__img', {interval: 20})
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__text', {interval: 20})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
-
-
-
-
